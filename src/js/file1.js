06ca6fb98b9513aac20a04a96f02f57a9888bd2f
Pavel545 <p_levin00@mail.ru>
2022-08-01 16:19:56 +0400

    step 2.2

--- src/js/file1.js
+++ src/js/file1.js
@@ -1,23 +1,56 @@
-function renderExampleMain(container, cls) {
+function renderExampleDiv(container, cls, content) {
     let main = {
       tag: "div",
       cls: `${cls}`,
+      content: `${content}`,
     };
     container.appendChild(templateEngine(main));
-  }
-window.application.blocks["example-main"] = renderExampleMain;
+}
+window.application.blocks["example-div"] = renderExampleDiv;
+function renderExampleButtonFalse(container, cls, content,id) {
+    let main = {
+      tag: "div",
+      cls: `${cls}`,
+      content: `${content}`,
+      attrs: {
+        id : id,
+      }
+    };
+    container.appendChild(templateEngine(main));
+}
+window.application.blocks["example-false"] = renderExampleButtonFalse;
+function renderExampleButton(container, cls, content) {
+    let main = {
+      tag: "button",
+      cls: `${cls}`,
+      content: `${content}`,
+    };
+    container.appendChild(templateEngine(main));
+}
+window.application.blocks["example-button"] = renderExampleButton;
 
 function renderExampleScreenStart() {
     const app = document.querySelector(".app");
-    app.textContent = "";
-  
-    window.application.renderBlock("example-main",app, 'main');
-  
-    window.application.renderBlock(
-      "example-button",
-      content,
-      "login_button",
-      "Login"
-    );
+    
+    window.application.renderBlock("example-div",app, 'main','');
+    const main = app.querySelector('.main');
+    window.application.renderBlock("example-div",main, 'complexity','');
+    
+    const div = main.querySelector('.complexity')
+    window.application.renderBlock("example-div" ,div, 'complexity_text','Выбери сложность');
+    window.application.renderBlock("example-div" ,div, 'complexity_choice_area','');
+    
+    const complexity_choice_area = div.querySelector('.complexity_choice_area');
+    window.application.renderBlock("example-false" ,complexity_choice_area, 'complexity_choice_button','1','easy');
+    window.application.renderBlock("example-false" ,complexity_choice_area, 'complexity_choice_button','2','average');
+    window.application.renderBlock("example-false" ,complexity_choice_area, 'complexity_choice_button','3','difficult');
+    
+    window.application.renderBlock("example-button" ,div, 'complexity_button','Старт');
   }
-  window.application.screens["example"] = renderExampleScreen;
+window.application.screens["example"] = renderExampleScreenStart;
+
+function renderExampleScreenGame() {
+    const app = document.querySelector(".app");
+    app.innerHTML = "";
+  }
+window.application.screens["game"] = renderExampleScreenGame;
